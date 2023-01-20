47977c72b7e93b61a8948c465c1832f525516bfa
Arpan Sunuwar <119127533+arpanrupacha@users.noreply.github.com>
2023-01-20 14:09:28 +0545

    Add files via upload

---
+++ Workshops/Week 9/js/script.js
@@ -0,0 +1,20 @@
+alert("Onii-Chan");
+console.log("Doom Eternals");
+console.log(document.getElementById("paragraph").innerHTML)
+document.getElementById("paragraph").innerHTML="<h1>Hello World!</h1>";
+/* document.getElementById("paragraph").style.color="purple";
+document.getElementById("paragraph").style.background="aqua"; */
+document.getElementById("paragraph").style.cssText="color=purple; Background=aqua";
+
+var age = 10;
+age = 13;
+const age1 = 10; /* can't reasign the value */
+// age1 = 90;
+let age3 = 10;
+age3 = 20;
+var name = "Jenny";
+
+console.log(age);
+console.log(age1);
+console.log(typeof age3);
+console.log(typeof name);
